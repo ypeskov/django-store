a9a04081d6b82a9f27a49cec888374381c6c54e6
Yuriy Peskov <yuriy.peskov@gmail.com>
2023-08-25 20:11:09 +0300

    not very init

---
+++ myshop/celery.py
@@ -0,0 +1,13 @@
+import os
+from celery import Celery
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
+app = Celery('myshop')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
